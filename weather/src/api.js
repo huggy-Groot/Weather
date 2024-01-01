ae18ca7cce3cd5b91fc1fb28dd151aadb52a7b62
Finally-Kwaku <kwakunagyamfi@gmail.com>
2024-01-01 17:27:30 +0000

    pop

---
+++ weather/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd64f289994mshca0f167262d3a62p179dbajsnd77fbe06fb27',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "057b52e858f2d8d689a9d7750fe28915";
---
+++ weather/src/components/current-weather/current-weather.js
@@ -0,0 +1,45 @@
+import "./current-weather.css"
+
+const CurrentWeather = ({ data }) => {
+  return (
+    <div className="weather">
+      <div className="top">
+	<div>
+	  <p className="city">{data.city}</p>
+	  <p className="weather-description">{data.weather[0].description}</p>
+	</div>
+	<img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${data.weather[0].icon}.png`}
+        />
+      </div>
+      <div className="bottom">
+	<p className="temperature">{Math.round(data.main.temp)}°C</p>
+	<div className="details">
+	  <div className="parameter-row">
+	    <span className="parameter-details">Details</span>
+	  </div>
+	  <div className="parameter-row">
+            <span className="parameter-details">feels like</span>
+	    <span className="parameter-value">{Math.round(data.main.feels_like)}</span>
+          </div>
+	  <div className="parameter-row">
+            <span className="parameter-details">Wind</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
+          </div>
+	  <div className="parameter-row">
+            <span className="parameter-details">Humidity</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
+          </div>
+	  <div className="parameter-row">
+            <span className="parameter-details">Pressure</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
+          </div>
+	</div>
+      </div>
+    </div>
+  );
+}
+
+export default CurrentWeather;
